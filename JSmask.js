3d665f1c657b072f49eb8294d441a1fee5571afc
Henri Azevedo <56831533+hnrazevedo@users.noreply.github.com>
2020-08-26 10:39:51 -0300

    Create JS file

---
+++ JSmask.js
@@ -0,0 +1,64 @@
+'use strict';
+
+const Mask = function() {
+    return {
+        masks:{
+            '#':'\\d',
+            'A':'[A-Z]',
+            'a':'[a-z]',
+            'S':'[a-zA-Z]',
+            'X':'[0-9a-zA-Z]'
+        },
+        start(){
+            if(document.querySelector('[data-mask]') != null){
+                document.querySelectorAll('[data-mask]').forEach(function(input,i){
+                    Mask.format('[data-mask="'+input.dataset.mask+'"]');
+                });
+            }
+        },
+        format(element) {            
+            var el = document.querySelector(element);
+            var maskForm = '';
+            
+            maskForm = el.dataset.mask;
+            el.maxLength = maskForm.length;
+            
+            el.addEventListener('keyup', function (e){
+                if (e.keyCode !== 8 && e.keyCode !== 46) { 
+                    costume(maskForm);
+                }
+            });
+            
+            function costume(mask) {
+                var value = '';
+                var data = el.value;
+                var c, m, i, x;
+
+                for (i = 0, x = 1; x && i < mask.length; ++i) {
+                    c = data.charAt(i);
+                    m = mask.charAt(i);
+
+                    if(Mask.masks[m] != undefined){
+                        if((new RegExp(Mask.masks[m])).test(c)){
+                            value += c;
+                            continue;
+                        }
+                    }else{
+                        value += m;
+                        continue;
+                    }
+                    x = 0;
+                }
+                el.value = value;                
+            }
+            
+        }
+    };
+}();
+
+document.addEventListener('DOMContentLoaded',function(){
+    Mask.start();
+    window.Mask = Mask;
+});
+
+export default Mask;
